5ca44a29eb1fa2f08ec7081384c6d61aa3f77407
Eduardo Guido <eduardo.guido@resolutte.com.br>
2025-04-28 15:28:39 -0300

    feat(day3-A): full domain (Money, Item, PriceHistory, AlertRule) + unit tests

---
+++ src/Backend/PriceWatch.Domain/Entities/AlertRule.cs
@@ -0,0 +1,24 @@
+using PriceWatch.Domain.ValueObjects;
+
+namespace PriceWatch.Domain.Entities;
+
+public sealed class AlertRule
+{
+    private AlertRule() { }
+
+    private AlertRule(Guid id, Guid itemId, Money targetPrice)
+    {
+        Id         = id;
+        ItemId     = itemId;
+        TargetPrice= targetPrice;
+        Enabled    = true;
+    }
+
+    public Guid  Id          { get; private set; }
+    public Guid  ItemId      { get; private set; }
+    public Money TargetPrice { get; private set; }
+    public bool  Enabled     { get; private set; }
+
+    public static AlertRule Create(Guid itemId, Money target) =>
+        new(Guid.NewGuid(), itemId, target);
+}
